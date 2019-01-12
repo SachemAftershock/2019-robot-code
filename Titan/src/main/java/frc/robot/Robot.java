066ccc74f67e8ce8290734d2a84f5b505e6a7397
rbapat <rorobapat@gmail.com>
2019-01-12 18:55:14 -0500

    Added call to SWDrive::drive in teleopPeriodic

--- Titan/src/main/java/frc/robot/Robot.java
+++ Titan/src/main/java/frc/robot/Robot.java
@@ -1,28 +1,16 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
+
+  XboxController primaryDriver, secondaryDriver;
+
   @Override
   public void robotInit() {
+    primaryDriver = new XboxController(Constants.PRIMARY_DRIVER_PORT);
+    secondaryDriver = new XboxController(Constants.SECONDARY_DRIVER_PORT);
   }
 
   @Override
@@ -39,6 +27,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    SWDrive.getInstance().drive(primaryDriver);
   }
 
   @Override
