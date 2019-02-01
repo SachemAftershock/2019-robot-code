b58d5572ac0aa1e5f4707baecddd46a284c1481e
rbapat <rorobapat@gmail.com>
2019-02-01 18:29:51 -0500

    Robot.java is weird

--- Titan/src/main/java/frc/robot/Robot.java
+++ Titan/src/main/java/frc/robot/Robot.java
@@ -2,64 +2,38 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import frc.robot.Enums.*;
-import frc.robot.Auton.*;
 
 public class Robot extends TimedRobot {
 
-  XboxController primaryDriver, secondaryDriver;
-  SWDrive drive;
-  Autonomous auto;
+  SWDrive driveBase;
   Compressor compressor;
-
-  boolean autoEnabled;
 
   @Override
   public void robotInit() {
-    primaryDriver = new XboxController(Constants.PRIMARY_DRIVER_PORT);
-    secondaryDriver = new XboxController(Constants.SECONDARY_DRIVER_PORT);
-    drive = SWDrive.getInstance();
-    auto = Autonomous.getInstance();
+    driveBase = SWDrive.getInstance();
+
     compressor = new Compressor();
     compressor.start();
     compressor.setClosedLoopControl(true);
-
-    autoEnabled = true;
-    drive.zero();
   }
 
   @Override
   public void autonomousInit() {
-    drive.zero();
-
-    auto.queueObjective(new AutonRotate(Rotation.CLOCKWISE, 90.0));
-    auto.queueObjective(new AutonDrive(Distance.FEET, 6.0));
+    driveBase.zero();
   }
 
   @Override
   public void autonomousPeriodic() {
-      if(autoEnabled) {
-        auto.drive();
-      } else {
-        drive.drive(primaryDriver);
-      }
-
-      if(primaryDriver.getAButton()) { //TODO: change toggle button
-        autoEnabled = false;
-      }
   }
 
   @Override
   public void teleopInit() {
-    drive.zero();//TODO: Remove this after testing
+ 
   }
 
   @Override
   public void teleopPeriodic() {
-    drive.drive(primaryDriver);
+    driveBase.drive();
   }
 
   @Override
